7a4a345015b3c77d53aa445382187f7d81419b70
github username <um_2003_8@hotmail.com>
2021-08-04 12:44:48 -0400

    create render that give option of canvas size

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter
 
 // An engine is a controller that manages updating the simulation of the world
 const engine = Engine.create()
+
 // world is an object that comes with engine
 const { world } = engine
+
+// The Matter.Render module is a simple canvas based renderer for visualising instances of Matter.Engine
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600
+    }
+})
