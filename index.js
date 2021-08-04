84adc2887b05501d5cb325c8c2194a76f9b2183f
github username <um_2003_8@hotmail.com>
2021-08-04 12:39:59 -0400

    create an engine and refrence world to it

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter 
+
+// An engine is a controller that manages updating the simulation of the world
+const engine = Engine.create()
+// world is an object that comes with engine
+const { world } = engine
